1348ac48e0bacd9d217d81b6747698f6b23d001b
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2019-10-05 20:53:05 -0500

    Funcional
    Al momento funciona el servicio de notificaciones y la barra de navegación ha sido creada.

---
+++ app/src/main/java/com/example/tecmas/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.tecmas;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.onesignal.OneSignal;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import com.example.tecmas.Menu.FragmentBecas;
+import com.example.tecmas.Menu.FragmentCalendario;
+import com.example.tecmas.Menu.FragmentEmergencias;
+import com.example.tecmas.Menu.FragmentInicio;
+import com.example.tecmas.Menu.FragmentMapaInteractivo;
+import com.example.tecmas.Menu.Fragmenttransporte;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Begin: OneSginal Code
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+        //End: OneSignalCode
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer= findViewById(R.id.drawer_layout);
+        NavigationView navigationView= findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle); toggle.syncState();
+        if(savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentInicio()).commit();
+            navigationView.setCheckedItem(R.id.navmenu_inicio);
+        }
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.navmenu_inicio:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentInicio()).commit();
+                break;
+            case R.id.navmenu_becas:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentBecas()).commit();
+                break;
+            case R.id.navmenu_calendario:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentCalendario()).commit();
+                break;
+            case R.id.navmenu_emergencias:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentEmergencias()).commit();
+                break;
+            case R.id.navmenu_transporte:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Fragmenttransporte()).commit();
+                break;
+            case R.id.navmenu_mapainteractivo:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentMapaInteractivo()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public  void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/tecmas/Menu/FragmentCalendario.java
@@ -0,0 +1,22 @@
+package com.example.tecmas.Menu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.tecmas.R;
+
+public class FragmentCalendario extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.menu_fragment_calendario, container, false);
+    }
+}
