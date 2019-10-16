54873ca570d95be6d0470445fed953be51043178
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2019-10-16 12:00:24 -0500

    Funcional
    La presente versión (a excepción del apartado transporte) funciona correctamente, ya se incorporó el apartado de visualización del SII y se hizo el cambio de iconos del apk.

--- app/src/main/java/com/example/tecmas/Menu/Section_SII/FragmentSII.java
+++ app/src/main/java/com/example/tecmas/Menu/Section_SII/FragmentSII.java
@@ -11,26 +11,42 @@ import android.webkit.WebViewClient;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.tecmas.R;
 
 public class FragmentSII extends Fragment {
 
     WebView browser;
+    SwipeRefreshLayout RefreshLayout;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View vista = inflater.inflate(R.layout.menu_fragment_sii, container, false);
 
+        //Creo el swipeview y lo enlazo con su vista
+        RefreshLayout=(SwipeRefreshLayout) vista.findViewById(R.id.menu_sii_SwipeRefreshLayout);
 
+        //Creo el webview y lo enlazo con su vista
         browser= (WebView) vista.findViewById(R.id.menu_sii_browser);
-        browser.setWebViewClient(new WebViewClient());
+        browser.setWebViewClient(new WebViewClient(){
+
+            //Desactivamos la animación del refreshview cuando se termine de cargar la pagina
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(browser, url);
+                RefreshLayout.setRefreshing(false);
+            }
+        });
+        //Habilito el uso de javascript
         browser.getSettings().setJavaScriptEnabled(true);
 
+        //Modifico el zoom para que se ajuste al movil
         browser.getSettings().setLoadWithOverviewMode(true);
         browser.getSettings().setUseWideViewPort(true);
-
+        //Habilito el zoom para modificar con gestos
         browser.getSettings().setSupportZoom(true);
 
         browser.getSettings().setBuiltInZoomControls(true);
@@ -39,6 +55,8 @@ public class FragmentSII extends Fragment {
 
          */
 
+        //Modifico el comportamiento del boton regresar del movil para que regrese a la pagina
+        //anterior del webview y no a una activity anterior.
         browser.setOnKeyListener(new View.OnKeyListener()
         {
 
@@ -64,8 +82,21 @@ public class FragmentSII extends Fragment {
                 return false;
             }
         });
-
+        //Cargo la url:
         browser.loadUrl("http://mictlantecuhtli.itmatamoros.edu.mx");
+
+        //Habilito el refresh del sitio cuando se "jala el webview hacia abajo"
+        RefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        browser.loadUrl( "javascript:window.location.reload( true )" );
+                    }
+
+                }
+
+
+        );
 
 
         return vista;
