ddf03896be99510047f3b98e4c27791c30798431
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2019-10-23 13:27:48 -0500

    revision

--- app/src/main/java/com/example/tecmas/Menu/Section_Inicio/FragmentInicio.java
+++ app/src/main/java/com/example/tecmas/Menu/Section_Inicio/FragmentInicio.java
@@ -11,14 +11,26 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.tecmas.InfoCards.InfoObjects;
 import com.example.tecmas.InfoCards.RecyclerAdapter;
 import com.example.tecmas.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentInicio extends Fragment {
+
+    String URL_DATA= "https://wordpresspruebas210919.000webhostapp.com/wp-json/wp/v2/posts/";
 
     @Nullable
     @Override
@@ -49,15 +61,38 @@ public class FragmentInicio extends Fragment {
 
     public List<InfoObjects> fill_with_data() {
 
-        List<InfoObjects> data = new ArrayList<>();
+        final List<InfoObjects> data = new ArrayList<>();
 
-        data.add(new InfoObjects(1,"Entrega de Reconocimientos y Constancias a Personal del Instituto Tecnológico de Matamoros", "El Departamento de Recursos Humanos convocó a personal Docente y de Apoyo a la Docencia para realizar la entrega de constancias, estímulos por antigüedad por 10, 15, 20 y 25 años de servicio", R.drawable.img1));
-        data.add(new InfoObjects(2,"El Campus Matamoros del TecNM Inicia Ciclo Escolar con 5,000 Estudiantes", "El Tecnológico Nacional de México campus Instituto Tecnológico de Matamoros celebró ceremonia de inicio del Ciclo Escolar 2019-2020 el lunes 19 de agosto en punto de las 10:00 hrs. en el Gimnasio Auditorio del plantel con la asistencia de 1,000 personas entre estudiantes, docentes, personal de apoyo a  la docencia e invitados especiales y de Honor.", R.drawable.ic_launcher_background));
-        data.add(new InfoObjects(3,"A nuestra gran comunidad del Instituto Tecnológico de Matamoros", "Da inicio un nuevo ciclo académico, con lo que reafirmamos nuestros objetivos y metas, fortaleciendo los proyectos de mejora continua que cada uno de quienes integramos al TecNM en Matamoros, mantenemos semestre a semestre como parte del compromiso educativo en la región.", R.drawable.img2));
-        data.add(new InfoObjects(4,"1er. Lugar en Área de Administración en Maratón de Conocimientos de ANFECA", "Estudiantes del programa educativo de Licenciatura en Administración del Tecnológico Nacional de México (TecNM)  campus Instituto Tecnológico de Matamoros ganaron el 1er. Lugar en la disciplina de Administración", R.drawable.img1));
-        data.add(new InfoObjects(5,"Celebramos 47 años de ser Territorio Gorila", "Con un contingente conformado por más de  1,000 jóvenes Estudiantes, Fundadores, Docentes y Personal de Apoyo a la Educación encabezado por la Maestra Mara Grassiel Acosta González, Directora del I.T.Matamoros y la Maestra Genoveva Hi González Directora de la Escuela Secundaria Técnica No. 4 “Gral. Lázaro Cárdenas del Río”, ", R.drawable.img2));
-        data.add(new InfoObjects(6,"El TecNM campus Matamoros Entrega 119 Títulos Profesionales", " El viernes 27 de septiembre de 2019 se realizó Ceremonia de Entrega de Títulos a egresados de 11 programas educativos, que acudieron en punto de las 18:00 hrs. al Centro de Información “Q.B.P. Andrés Ortega Guzmán”;", R.drawable.img1));
+        StringRequest Datos = new StringRequest(Request.Method.GET, URL_DATA,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
+                        try {
+
+                          JSONArray array = new JSONArray(response);
+                          for(int i=0; i<array.length();i++){
+                              JSONObject o = array.getJSONObject(i);
+                              data.add(new InfoObjects(o.getInt("id"),o.getString("title"),"",R.drawable.ic_launcher_background));
+
+                          }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(Datos);
+        
         return data;
     }
 }
